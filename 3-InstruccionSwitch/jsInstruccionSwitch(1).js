b9de43cf585d2294edd2c694f9e26150bb8cf2cd
Santiago Mendez <mendezsantiago95@gmail.com>
2019-02-07 16:41:18 -0300

    Switch-ej1

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,8 +1,26 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
+	switch (mesDelAño)
+	{
+		case "Enero":{
+			alert("Que comiencies bien el año!");
+			break;
+		}
+		case "Marzo":{
+			alert("A clases!");
+			break;
+		}
+		case "Julio":{
+			alert("Se vienen las vacaciones!");
+			break;
+		}
+		case "Diciembre":{
+			alert("Felices fiestas!");
+			break;
+		}
+	} //Fin del Switch
 
 
 
