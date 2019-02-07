4e18982de73613213e2cc1573c2c5f611b50a168
Santiago Mendez <mendezsantiago95@gmail.com>
2019-02-07 15:35:45 -0300

    Cambio2

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,16 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
+	var nota;
+
+	nota = Math.round(Math.random()*10);  //Genero el número RANDOM entre 1 y 10 
 	
+	if (nota == 9 || nota == 10){
+		alert ("Excelente");
+	}
+	else if (nota >= 4){
+		alert ("Aprobó");
+	}else{
+		alert ("Vamos, la proxima se puede");
+	}
 
 }//FIN DE LA FUNCIÓN
