7a70c9462ad767c9ab7e2ab14095a8cac5c69f43
Santiago Mendez <mendezsantiago95@gmail.com>
2019-02-16 19:21:45 -0300

    Parcial ej5

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,26 @@
 function mostrar()
 {
+	var planet = prompt("Ingrese nombre del planeta: ");
 
-}
+	switch (planet){
+
+		case "mercurio":
+		case "venus":
+			alert("Acá hace más calor");
+			break;
+		case "tierra":
+			alert("Acá vivimos");
+			break;
+		case "marte":
+		case "jupiter":
+		case "saturno":
+		case "urano":
+		case "neptuno":
+			alert("Acá hace más frío");
+			break;
+		default:
+			alert("No es un planeta válido");
+
+	}//Fin del switch
+
+}//Fin del prog
